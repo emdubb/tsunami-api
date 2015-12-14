5da3b50b2ea8127d459f2cb60d4e5df5554c9ddd
Melissa Wilcox <melissa@emdubb.co>
2015-12-14 10:06:01 -0800

    set up associations between models

--- tsunami_api/app/models/map.rb
+++ tsunami_api/app/models/map.rb
@@ -1,2 +1,5 @@
 class Map < ActiveRecord::Base
+  has_one :legend
+  has_many_and_belongs_to :users
+  has_many_and_belongs_to :pins
 end
--- tsunami_api/app/models/user.rb
+++ tsunami_api/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_one :legend, through :maps
+  has_and_belongs_to_many :maps
+  has_many :pins, through :maps
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
