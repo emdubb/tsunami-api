e8ecc3db46a1986498de2c00d69076513c6cee76
Melissa Wilcox <melissa@emdubb.co>
2015-12-14 17:15:54 -0800

    use password digest

--- tsunami_api/app/models/user.rb
+++ tsunami_api/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :legends, through: :maps
   has_and_belongs_to_many :maps
   has_many :pins, through: :maps
-  # has_secure_password
-  # validates :email, presence: true, uniqueness: true
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
