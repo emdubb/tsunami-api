6bb602077e2669201e97db815376816793777d84
Melissa Wilcox <melissa@emdubb.co>
2015-12-16 11:23:25 -0800

    add params and create method

---
+++ tsunami_api/app/controllers/api/cities_controller.rb
@@ -0,0 +1,20 @@
+class City < ActiveRecord::Base
+
+  #POST /api/cities
+  def create
+    city = City.create(city_params)
+
+    if city.save
+      render json: city
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  private
+    def city_params
+      params.require(:city).permit(:name, :county, :twitter, :email,
+        :website1, :website2, :website3, :mobile_app, :facebook, :staion)
+    end
+
+end
