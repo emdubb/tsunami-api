12351cd04e2aa30194145f97cb26134b96d95286
Melissa Wilcox <melissa@emdubb.co>
2015-12-16 11:22:39 -0800

    require auth_token

--- tsunami_api/app/controllers/application_controller.rb
+++ tsunami_api/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'auth_token'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
