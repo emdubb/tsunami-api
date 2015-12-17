4c4506241e9f6fe69c7d881deef3bd445801e0b9
Melissa Wilcox <melissa@emdubb.co>
2015-12-16 18:50:47 -0800

    nest pins routes in user routes

--- tsunami_api/config/routes.rb
+++ tsunami_api/config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'api#index'
 
   namespace :api do
-    resources :users, only: [:index, :show, :create, :update, :destroy]
+    resources :users, only: [:index, :show, :create, :update, :destroy] do
+      resources :pins, only: [:create, :update, :destroy]
+    end
     resources :maps, only: [:index, :show, :create, :update, :destroy]
     resources :cities, only: [:index, :show, :create, :update, :destroy]
-    resources :pins, only: [:index, :show, :create, :update, :destroy]
 
     get 'me', to: 'users#me'
     post 'token', to: 'users#token'
