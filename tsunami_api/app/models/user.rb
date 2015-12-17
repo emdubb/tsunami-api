41c3110267dccad112f0e62d6008faaca666dd87
Melissa Wilcox <melissa@emdubb.co>
2015-12-16 18:21:58 -0800

    update pin relationship

--- tsunami_api/app/models/user.rb
+++ tsunami_api/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :maps, uniq: true
-  has_many :pins, through: :maps, dependent: :destroy
+  has_many :pins, dependent: :destroy
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
