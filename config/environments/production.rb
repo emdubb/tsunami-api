c069826afdbd914b53504a3c148084037290b4ef
Melissa Wilcox <melissa@emdubb.co>
2016-01-24 20:18:08 -0800

    add logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.log_level = :info
+  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
 
 
   # Do not dump schema after migrations.
